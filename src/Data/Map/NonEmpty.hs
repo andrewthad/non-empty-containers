a8293c40382983622cea9703da7dc841093e016c
Andrew Martin <andrew.thaddeus@gmail.com>
2018-06-23 15:31:32 -0400

    Foldable1 instance for NonEmptyMap

--- src/Data/Map/NonEmpty.hs
+++ src/Data/Map/NonEmpty.hs
@@ -6,14 +6,18 @@ module Data.Map.NonEmpty
   , lookup
   , foldl1'
   , foldr1'
+  , 
   ) where
 
-import Prelude hiding (lookup)
+import Prelude hiding (lookup,foldr1,foldr)
 import Data.Bool (bool)
-import Data.Foldable
+import Data.Semigroup.Foldable (Foldable1)
+import Data.List.NonEmpty (NonEmpty((:|)))
 
+import qualified Data.Foldable as F
 import qualified Data.Map as M
 import qualified Data.Map.Strict as MS
+import qualified Data.Semigroup.Foldable
 
 -- | A non-empty map.
 data NonEmptyMap k v = NonEmptyMap !k v !(M.Map k v)
@@ -37,24 +41,40 @@ unionAppend (NonEmptyMap xk xv xs) (NonEmptyMap yk yv ys) = case compare xk yk o
   LT -> NonEmptyMap xk xv (MS.unionWith (<>) xs (M.insertWith (<>) yk yv ys))
   GT -> NonEmptyMap yk yv (MS.unionWith (<>) (M.insertWith (<>) xk xv xs) ys)
 
+foldr1 :: (v -> v -> v) -> NonEmptyMap k v -> v
+foldr1 f (NonEmptyMap _ v m) = case M.maxView m of
+  Nothing -> v
+  Just (y,m') -> f v (M.foldr f y m')
+
+foldMap1 :: Semigroup m => (v -> m) -> NonEmptyMap k v -> m
+foldMap1 f (NonEmptyMap _ v m) = case M.maxView m of
+  Nothing -> f v
+  Just (x,m') -> f v <> M.foldr (\c d -> f c <> d) (f x) m'
+
 instance (Ord k, Semigroup v) => Semigroup (NonEmptyMap k v) where
   (<>) = unionAppend
 
 instance Foldable (NonEmptyMap k) where
-  fold (NonEmptyMap _ v m) = v <> fold m
+  fold (NonEmptyMap _ v m) = v <> F.fold m
   foldMap f (NonEmptyMap _ v m) = f v <> foldMap f m
   foldl f a (NonEmptyMap _ v m) = M.foldl f (f a v) m
   foldr f a (NonEmptyMap _ v m) = case M.maxView m of
     Nothing -> f v a
     Just (y,m') -> f v (M.foldr f (f y a) m')
   foldl1 f (NonEmptyMap _ v m) = M.foldl f v m
-  foldr1 f (NonEmptyMap _ v m) = case M.maxView m of
-    Nothing -> v
-    Just (y,m') -> f v (M.foldr f y m')
+  foldr1 = foldr1
   length (NonEmptyMap _ _ m) = 1 + M.size m
   null _ = False
   minimum = foldl1' min
   maximum = foldl1' max
+
+instance Foldable1 (NonEmptyMap k) where
+  toNonEmpty = elems
+  fold1 = foldr1 (<>)
+  foldMap1 = foldMap1
+
+elems :: NonEmptyMap k v -> NonEmpty v
+elems (NonEmptyMap _ v m) = v :| M.elems m
 
 foldl1' :: (v -> v -> v) -> NonEmptyMap k v -> v
 foldl1' f (NonEmptyMap _ v m) = M.foldl' f v m
